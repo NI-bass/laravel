e17599709a8286a5b17ab1971dedba3245e28525
NI-bass <naotoiogarashi34@gmail.com>
2023-04-02 17:23:36 +0900

    merge2

--- Vue_test/resources/views/test.blade.html.php
+++ Vue_test/resources/views/test.blade.html.php
@@ -1,13 +1,3 @@
 <?php
-echo "test";
-echo "test2";
-echo "test4";
-echo "test5";
-echo "test6";
-echo "test7";
-echo "test8";
-echo "test9";
-echo "test10";
-echo "test11";
-echo "test27";
+echo "test_merge";
 ?>
